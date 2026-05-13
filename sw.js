5ab8a239351bb795feadeb760079943f37000fd3
mochiko-50202 <mochiko-50202@users.noreply.github.com>
2026-05-14 03:06:41 +0900

    Restore original Gemini icon + remove maskable purpose

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kanji-shoukan-v3';
+const CACHE = 'kanji-shoukan-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon.png', './dictionary.js'];
 
 self.addEventListener('install', e => {
